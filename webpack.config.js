d49115f19106258256f5db650de8b6b7af90d29c
anilgudur <anilg.cse@gmail.com>
2019-01-25 23:01:05 +0530

    22.373 - Uptu Rules for Images

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: ""
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            importLoaders: 1,
+            loader: 'css-loader',
+            // Below setting not needed as in latest React JS it is allowing
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  "browsers": [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]"
+      }
+    ]
+  }
+};
